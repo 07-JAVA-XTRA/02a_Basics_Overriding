4c6a51ff352af008880d2ba4db6441ed8220f086
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-03-10 15:23:29 +0000

    Basics

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,9 +1,23 @@
 package com.cc.java;
 
+
 public class App {
     
     public static void main(String[] args) {
      
+        Child child = new Child("John");
+        Child child2 = new Child("Mary");
+
+        System.out.println(child);
+        System.out.println(child2);
+
+        System.out.println(child.getName());
+        System.out.println(child2.getName());
+      
+
+        
+
+
     }
 
 }
---
+++ com/cc/java/Child.java
@@ -0,0 +1,25 @@
+package com.cc.java;
+
+public class Child {
+    
+ private String name;
+
+ public Child(String name){
+     this.name = name;
+ }
+
+public String getName() {
+    return name;
+}
+
+
+
+ 
+
+
+ 
+
+
+
+
+}
