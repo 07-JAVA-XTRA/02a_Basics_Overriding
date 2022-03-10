6b36db65027354861059409a53c7c91aac355332
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-03-10 15:39:52 +0000

    Objekte / Instanzen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -7,12 +7,15 @@ public class App {
      
         Child child = new Child("John");
         Child child2 = new Child("Mary");
+        Child child3 = new Child("Maria"); 
 
         System.out.println(child);
         System.out.println(child2);
-
+        System.out.println(child3);
+        
         System.out.println(child.getName());
         System.out.println(child2.getName());
+        System.out.println(child3.getName());
       
 
         
